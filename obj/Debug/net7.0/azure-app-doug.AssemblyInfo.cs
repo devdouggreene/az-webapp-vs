7b877c278a7b14f65529e96824939cb98b86f62e
gvisor <gvisor@gmail.com>
2023-12-27 11:10:29 -0500

    Fixed Privay Page
    Signed-off-by: gvisor <gvisor@gmail.com>

--- obj/Debug/net7.0/azure-app-doug.AssemblyInfo.cs
+++ obj/Debug/net7.0/azure-app-doug.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure-app-doug")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ca0ecc7031cf3d60370f8d5bdeb265fa4312af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure-app-doug")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure-app-doug")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
